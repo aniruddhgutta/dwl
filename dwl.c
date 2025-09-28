e415084b4047378c4ba03040309d8ac7a6c90765
aniruddhgutta <aniruddhgutta@disroot.org>
2025-09-28 08:30:53 +0530

    feat(scratchpad): merged simple_scratchpad.c into dwl.c

--- dwl.c
+++ dwl.c
@@ -530,8 +530,6 @@ static struct wlr_xwayland *xwayland;
 /* attempt to encapsulate suck into one file */
 #include "client.h"
 
-#include "simple_scratchpad.c"
-
 static pid_t *autostart_pids;
 static size_t autostart_len;
 
@@ -1764,6 +1762,63 @@ drawbars(void)
 
 	wl_list_for_each(m, &mons, link)
 		drawbar(m);
+}
+
+void
+addscratchpad(const Arg *arg)
+{
+	Client *cc, *c = focustop(selmon);
+
+	if (!c)
+		return;
+	/* Check if the added client is already a scratchpad client */
+	wl_list_for_each(cc, &scratchpad_clients, link_temp) {
+		if (cc == c)
+			return;
+	}
+	if (!c->isfloating) {
+		setfloating(c, 1);
+	}
+	wl_list_insert(&scratchpad_clients, &c->link_temp);
+}
+
+void
+togglescratchpad(const Arg *arg)
+{
+	Client *c;
+	Monitor *m = selmon;
+
+	scratchpad_visible = !scratchpad_visible;
+	if (scratchpad_visible) {
+		wl_list_for_each(c, &scratchpad_clients, link_temp) {
+			c->mon = m;
+			c->tags = m->tagset[m->seltags];
+			arrange(m);
+			focusclient(c, 1);
+		}
+	} else {
+		wl_list_for_each(c, &scratchpad_clients, link_temp) {
+			c->tags = 0;
+			focusclient(focustop(m), 1);
+			arrange(m);
+		}
+	}
+}
+
+void
+removescratchpad(const Arg *arg)
+{
+	Client *sc, *c = focustop(selmon);
+
+	if (c && wl_list_length(&scratchpad_clients) > 0) {
+		/* Check if c is in scratchpad_clients */
+		wl_list_for_each(sc, &scratchpad_clients, link_temp) {
+			if (sc == c) {
+				wl_list_remove(&c->link_temp);
+				break;
+			}
+		}
+	}
 }
 
 void
